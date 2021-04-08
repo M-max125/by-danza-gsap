886760aff0f1b8f41407a795e44b4364ae3a07f7
M-max125 <c.marcu60@yahoo.com>
2021-04-08 22:45:11 +0300

    iframe fixed

--- index.php
+++ index.php
@@ -393,16 +393,16 @@ $key = getenv(API_KEY);
     <p class="bd-adress">Our garage is in Ambassador Ave, Beverly Hills</p>
    </div>
 
-  <?php 
-   echo  '<iframe
+ 
+   <iframe
   width="100%"
   height="450"
   frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?key='.$key.'
-    &q=Ambassador Ave, Beverly Hills" allowfullscreen>';
+  src="https://www.google.com/maps/embed/v1/place?key=<?= $key;?>
+    &q=Ambassador Ave, Beverly Hills" allowfullscreen>
 
-   echo '</iframe>';
-  ?>
+  </iframe>
+ 
   
 
  
