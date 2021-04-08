d83ae8deaebf8e07fd1f848df09d6e32022705a0
M-max125 <c.marcu60@yahoo.com>
2021-04-08 22:56:01 +0300

    fixed slashes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
-$key = getenv(API_KEY);
-print_r($key);
+$key = getenv('API_KEY');
+
 
 ?>
 
@@ -395,14 +395,14 @@ print_r($key);
    </div>
 
  
-   <!-- <iframe
+   <iframe
   width="100%"
   height="450"
   frameborder="0" style="border:0"
   src="https://www.google.com/maps/embed/v1/place?key=<?= $key;?>
     &q=Ambassador Ave, Beverly Hills" allowfullscreen>
 
-  </iframe> -->
+  </iframe>
  
   
 
