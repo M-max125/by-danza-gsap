0b5c62d1b74595554e4883633ce9825805677ec0
M-max125 <c.marcu60@yahoo.com>
2021-04-08 22:37:24 +0300

    changed iframe

--- index.php
+++ index.php
@@ -398,7 +398,7 @@ $key = getenv(API_KEY);
   width="100%"
   height="450"
   frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?'.$key.'
+  src="https://www.google.com/maps/embed/v1/place?key='.$key.'
     &q=Ambassador Ave, Beverly Hills" allowfullscreen>';
 
    echo '</iframe>';
