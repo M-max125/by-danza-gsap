2c2c7d0381d8fe111631e7ee0772cc822418a3f6
M-max125 <c.marcu60@yahoo.com>
2021-04-08 22:48:23 +0300

    test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
 $key = getenv(API_KEY);
+print_r($key);
 
 ?>
 
@@ -394,14 +395,14 @@ $key = getenv(API_KEY);
    </div>
 
  
-   <iframe
+   <!-- <iframe
   width="100%"
   height="450"
   frameborder="0" style="border:0"
   src="https://www.google.com/maps/embed/v1/place?key=<?= $key;?>
     &q=Ambassador Ave, Beverly Hills" allowfullscreen>
 
-  </iframe>
+  </iframe> -->
  
   
 
